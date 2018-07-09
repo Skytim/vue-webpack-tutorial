b557691c1dc237b143d1b425d68fb3cfabe91575
蔡宇祥 <sky22357168@gmail.com>
2018-07-10 00:08:34 +0800

    Add local types & i18n

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 // import './styles/index.scss';
+import locale from 'element-ui/lib/locale/lang/en';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import VueI18n from 'vue-i18n';
 import '@babel/polyfill';
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 Vue.use(VueI18n);
 Vue.config.productionTip = false;
 
